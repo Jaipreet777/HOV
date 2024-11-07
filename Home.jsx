c39e5ddbdf62c8c368fbd940acb891025a36d631
Jay Dhillon <131726190+Jaipreet777@users.noreply.github.com>
2024-11-06 23:59:43 -0700

    homepage updated

--- Home.jsx
+++ Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import NewArrivals from '../components/NewArrivals'
 import OurCollection from '../components/OurCollection'
 import BestSellers from '../components/BestSellers'
+import ContactUs from '../components/ContactUs'
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
             <NewArrivals/>
             <OurCollection/>
             <BestSellers/>
+            <ContactUs/>
         </div>
     )
 }
