7cac8dfbd52f74e4de896ac884655db3fbd71e1b
Jay Dhillon <131726190+Jaipreet777@users.noreply.github.com>
2024-11-06 23:56:51 -0700

    Homepage updated

--- Home.jsx
+++ Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import NewArrivals from '../components/NewArrivals'
+import OurCollection from '../components/OurCollection'
+import BestSellers from '../components/BestSellers'
+
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <NewArrivals/>
+            <OurCollection/>
+            <BestSellers/>
         </div>
     )
 }
