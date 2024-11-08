99f317c578aaf4feeda73c769aa06e1192c6b89c
Pawandeepkaur05 <120079621+Pawandeepkaur05@users.noreply.github.com>
2024-11-07 23:10:11 -0700

    importing done

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React,{useContext, useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
 
 const product = () => {
   return (
