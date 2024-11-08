9760f3a41638270171bf2926608e65a85507c77b
Ravneets13 <141952548+Ravneets13@users.noreply.github.com>
2024-11-07 21:05:12 -0700

    UPDATED

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vm] md:px-[7vm] 1g:px-[9vm]'>
       
+      <Routes>
+       
+      </Routes>
     </div>
   )
 }
