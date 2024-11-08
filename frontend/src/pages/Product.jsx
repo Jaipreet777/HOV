f5625bcee45d6b37ea0940363b4a09376a29de20
Pawandeepkaur05 <120079621+Pawandeepkaur05@users.noreply.github.com>
2024-11-07 23:20:12 -0700

    product updATED

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -28,6 +28,26 @@ const product = () => {
     <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
    {/*---images of products below  code -*/}
+   <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {productData.image.map((item, index) => (<img key={index} onClick={() => setImage(item)} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' src={item} alt="" />))}
+          </div>
+          <div className='w-full sm:w-[80%]'>
+            <img className='w-full h-auto' src={image} alt="" />
+          </div>
+        </div>
+        <div className='flex-1'>
+
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+          <div className='flex items-center gap-1 mt-2'>
+            <img className='w-3.5' src={assets.star_icon} alt="" />
+            <img className='w-3.5' src={assets.star_icon} alt="" />
+            <img className='w-3.5' src={assets.star_icon} alt="" />
+            <img className='w-3.5' src={assets.star_icon} alt="" />
+            <img className='w-3.5' src={assets.star_dull_icon} alt="" />
+            <p className='pl-2'>(122)</p>
+          </div>
+
       </div>
       
     </div>
