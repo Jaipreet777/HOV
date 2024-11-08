480a6d751a5602d7591df6d0be7db458065fe81f
Pawandeepkaur05 <120079621+Pawandeepkaur05@users.noreply.github.com>
2024-11-07 22:31:46 -0700

    NAVLINK CREATED

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,10 +8,21 @@ const Navbar = () => {
       <img src={assets.logo} className='w-36'alt=''/>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-        <NavLink className='flex flex-col items-center gap-1'>
+        <NavLink to='/'className='flex flex-col items-center gap-1'>
             <p>Home</p>
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
-
+        </NavLink>
+        <NavLink to='/'className='flex flex-col items-center gap-1'>
+            <p>Login</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+        </NavLink>
+        <NavLink to='/'className='flex flex-col items-center gap-1'>
+            <p>Product</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+        </NavLink>
+        <NavLink to='/'className='flex flex-col items-center gap-1'>
+            <p>Collection</p>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
         </NavLink>
 
       </ul>
