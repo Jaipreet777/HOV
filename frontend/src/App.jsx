579e2fb6d5654466786b56bd403fe3137b31c36d
Jay Dhillon <131726190+Jaipreet777@users.noreply.github.com>
2024-11-08 00:22:32 -0700

    home (imported)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Collection from './pages/Collection'
-import Product from '../pages/Product'
-import Navbar from './components/Navbar'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import Product from "../pages/Product";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vm] md:px-[7vm] 1g:px-[9vm]'>
+    <div className="px-4 sm:px-[5vm] md:px-[7vm] 1g:px-[9vm]">
       <Navbar />
-      <Routes >
-        <Route path='/collection' element={<Collection />} />
-        <Route path='/product/:productId' element={<Product />} />
+      <Routes>
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/product/:productId" element={<Product />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 //REf: https://chatgpt.com/
-// How we reslove the issue when the route is not importing correctly 
-//Ref: www.youtube.com for learning how routes work and react toastify 
+// How we reslove the issue when the route is not importing correctly
+//Ref: www.youtube.com for learning how routes work and react toastify
