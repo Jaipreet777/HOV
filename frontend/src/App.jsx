dff8aa3e3c46d2aa77ffb836ef787e452295b001
Ravneets13 <141952548+Ravneets13@users.noreply.github.com>
2024-11-08 00:02:18 -0700

    del

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
 import Collection from './pages/Collection'
 import Product from '../pages/Product'
 import Navbar from './components/Navbar'
