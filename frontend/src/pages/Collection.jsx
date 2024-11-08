82991fcf623cfa86b61564c544dba27bdbe36ff6
Ravneets13 <141952548+Ravneets13@users.noreply.github.com>
2024-11-07 23:41:37 -0700

    imortinf g done

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Title from '../componnets/Title'
+import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
+
 
 const Collection = () => {
   return (
