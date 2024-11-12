96e2503f89a1a7651e0eb5bb443f44db9015c69c
Jaipreet777 <131726190+Jaipreet777@users.noreply.github.com>
2024-11-11 22:36:26 -0700

    js updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,12 @@
 import { initializeApp }  from 'firebase/app';
 
-const firebaseApp = initializeApp ({});
+const firebaseConfig = {
+    apiKey: "AIzaSyAlcuwwvy3wQp0jc9ZVK3k3ncd-mOtq8bg",
+    authDomain: "hov24-c7777.firebaseapp.com",
+    projectId: "hov24-c7777",
+    storageBucket: "hov24-c7777.firebasestorage.app",
+    messagingSenderId: "817065914087",
+    appId: "1:817065914087:web:21e20e825c5dfa356fbc42",
+    measurementId: "G-P6CYCFFRB4"
+};
+    
