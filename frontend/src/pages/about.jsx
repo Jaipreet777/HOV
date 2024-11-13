167f1195cc9609249ae7b65072fc670818f12b03
Jaipreet777 <131726190+Jaipreet777@users.noreply.github.com>
2024-11-13 02:17:32 -0700

    au

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -4,5 +4,11 @@ import { assets } from "../assets/assets";
 import NewsletterBox from "../components/NewletterBox";
 
 const About = () => {
-    return()
-}
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+    </div>
+  );
+};
