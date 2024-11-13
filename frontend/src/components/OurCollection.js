94f6f9bad6ea72aab45f506cd6140cefb8c6d5e8
Jaipreet777 <131726190+Jaipreet777@users.noreply.github.com>
2024-11-13 00:28:27 -0700

    oc

---
+++ frontend/src/components/OurCollection.js
@@ -0,0 +1,13 @@
+//src/components/OurCollection.js
+
+import React from "react"
+
+const OurCollection = () => {
+    return(
+        <section className="our-collection">
+            <h2>Our Collection</h2>
+        </section>
+    );
+};
+
+export default OurCollection;
