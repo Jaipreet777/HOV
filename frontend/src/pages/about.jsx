91e8fd483620a59547a978d685987f141a926df3
Jaipreet777 <131726190+Jaipreet777@users.noreply.github.com>
2024-11-13 02:40:25 -0700

    au6

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -42,6 +42,15 @@ const About = () => {
       <div className="text-xl py-4">
         <Title text1={"WHY"} text2={"CHOOSE US"} />
       </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
