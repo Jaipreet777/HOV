4ad79fb2f4cb846dbdd93bf48570e3664f54b2f3
Jaipreet777 <131726190+Jaipreet777@users.noreply.github.com>
2024-11-13 00:22:54 -0700

    na

---
+++ frontend/src/components/NewArrivals.js
@@ -0,0 +1,13 @@
+//src/components/NewArrivals.js
+
+import React from "react";
+
+const NewArrivals = () => {
+    return (
+        <section className="new-arrivals">
+            <h2>New Arrivals</h2>
+        </section>
+    );
+};
+
+export default NewArrivals;
