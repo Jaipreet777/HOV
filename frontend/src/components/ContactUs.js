569be1ee5e842b685e27ca89324b7a22cb51d495
Jaipreet777 <131726190+Jaipreet777@users.noreply.github.com>
2024-11-13 00:36:13 -0700

    cu

---
+++ frontend/src/components/ContactUs.js
@@ -0,0 +1,13 @@
+//src/components/ContactUs.js
+
+import React from "react";
+
+const ContactUs = () => {
+    return (
+        <section className="contact-us">
+            <h2>Contact Us</h2>
+        </section>
+    );
+};
+
+export default ContactUs;
