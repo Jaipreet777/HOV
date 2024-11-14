4190508f52c4dd6fa3308eaca07aab14cdb0af14
Pawandeepkaur05 <120079621+Pawandeepkaur05@users.noreply.github.com>
2024-11-14 15:18:27 -0700

    importing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import Footer from './components/Footer'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import SearchBar from './components/SearchBar'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
