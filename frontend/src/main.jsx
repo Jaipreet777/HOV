f4e5ba758f828ee438961124f68b34061ad6881a
Ravneets13 <141952548+Ravneets13@users.noreply.github.com>
2024-11-14 15:37:47 -0700

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,3 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+    </BrowserRouter>
+)
