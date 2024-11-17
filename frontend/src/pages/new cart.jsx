20c4897fccce2af9f295e16a494e9d5a66c19c10
gurnoorjatana2 <gurnoor.jatana.2@gmail.com>
2024-11-17 13:48:13 -0700

    update

--- frontend/src/pages/new cart.jsx
+++ frontend/src/pages/new cart.jsx
@@ -2,3 +2,4 @@ import React, { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import { Link } from 'react-router-dom';
 const CartPage = () => {
+    const { cart, updateCartItemQuantity, removeCartItem, currency } = useContext(ShopContext);
