c262dc57565b7417dd136573fd4c4d0e02ba39c5
gurnoorjatana2 <gurnoor.jatana.2@gmail.com>
2024-11-17 13:04:10 -0700

    update

---
+++ frontend/src/pages/related.jsx
@@ -0,0 +1,9 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const RelatedProducts = ({category,subCategory}) => {
+
+    const { products } = useContext(ShopContext);
+    const [related,setRelated] = useState([]); }
