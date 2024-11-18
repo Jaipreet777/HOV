8b294f723ce1ac1db16111b06116cbcaf70e4df0
gurnoorjatana2 <gurnoor.jatana.2@gmail.com>
2024-11-17 19:36:53 -0700

    update

--- frontend/src/pages/add product new.jsx
+++ frontend/src/pages/add product new.jsx
@@ -3,3 +3,10 @@
 import React, { useState } from 'react';
 import { db } from '../firebase'; // Import your Firestore reference
 import { collection, addDoc } from 'firebase/firestore'; // Firestore functions
+const categories = ['Electronics', 'Fashion', 'Home Appliances', 'Books', 'Toys', 'Beauty', 'Sports'];
+
+export default function AddProduct() {
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('');
+  const [price, setPrice] = useState('');
+  const [stock, setStock] = useState('');}
